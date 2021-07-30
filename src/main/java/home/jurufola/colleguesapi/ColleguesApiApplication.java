d3092d188377bdc42938699403ca6fc1587a5877
jurufola <moulaye.haidara@laposte.net>
2021-07-30 19:17:26 +0200

    Initial commit

---
+++ src/main/java/home/jurufola/colleguesapi/ColleguesApiApplication.java
@@ -0,0 +1,13 @@
+package home.jurufola.colleguesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColleguesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColleguesApiApplication.class, args);
+	}
+
+}
